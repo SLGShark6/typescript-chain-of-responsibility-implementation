8f1155ae8dc4d170ee3948b96e7dd850515c1b3e
SLGShark6 <gardnershane@outlook.com>
2020-11-06 05:21:23 -0500

    Make it clear that the list is reversed for creating the request pipeline

--- index.ts
+++ index.ts
@@ -1,46 +1,38 @@
 // Import stylesheets
-import './style.css';
-
+import "./style.css";
 
 type SentenceHandlerDelegate = (sentence: string) => string;
 
-// 
+//
 interface ISentenceHandler {
-
-    handle(sentence: string, next: SentenceHandlerDelegate): string;
+  handle(sentence: string, next: SentenceHandlerDelegate): string;
 }
 
 /////////////////////////////////////////////////////////////
 // Test Classes
 
 class HelloWordHandler implements ISentenceHandler {
+  handle(sentence: string, next: SentenceHandlerDelegate): string {
+    sentence = `${sentence} Hello`;
 
-    handle(sentence: string, next: SentenceHandlerDelegate): string {
-        sentence = `${sentence} Hello`;
-
-        return next(sentence);
-    }
-
+    return next(sentence);
+  }
 }
 
 class ThereWordHandler implements ISentenceHandler {
+  handle(sentence: string, next: SentenceHandlerDelegate): string {
+    sentence = `${sentence} There`;
 
-    handle(sentence: string, next: SentenceHandlerDelegate): string {
-        sentence = `${sentence} There`;
-
-        return next(sentence);
-    }
-
+    return next(sentence);
+  }
 }
 
 class WorldWordHandler implements ISentenceHandler {
+  handle(sentence: string, next: SentenceHandlerDelegate): string {
+    sentence = `${sentence} World!`;
 
-    handle(sentence: string, next: SentenceHandlerDelegate): string {
-        sentence = `${sentence} World!`;
-
-        return next(sentence);
-    }
-
+    return next(sentence);
+  }
 }
 
 ////////////////////////////////////////////////////////
@@ -49,37 +41,40 @@ class WorldWordHandler implements ISentenceHandler {
  * Required because of how context works with creating inline functions with Javascript.
  * So the nextDelegate is copied by value as well and not reference.
  */
-function createDelegate(handler: ISentenceHandler, nextDelegate: SentenceHandlerDelegate): SentenceHandlerDelegate {
-    // Create and return handler delegate
-    return (sentence: string) => {
-        return handler.handle(sentence, nextDelegate);
-    }
+function createDelegate(
+  handler: ISentenceHandler,
+  nextDelegate: SentenceHandlerDelegate
+): SentenceHandlerDelegate {
+  // Create and return handler delegate
+  return (sentence: string) => {
+    return handler.handle(sentence, nextDelegate);
+  };
 }
 
 // Define the process chain
 let sentenceProcessChain: ISentenceHandler[] = [
-    new HelloWordHandler(),
-    new ThereWordHandler(),
-    new WorldWordHandler()
+  new HelloWordHandler(),
+  new ThereWordHandler(),
+  new WorldWordHandler()
 ];
 
 // Define the delegate chain from a bottom up perspective
 let nextDelegate: SentenceHandlerDelegate = (sentence: string) => {
-    return sentence;
-}
+  return sentence;
+};
 
 // And work our way to the very first function delegate to call
-sentenceProcessChain.reverse().forEach(processor => {
+sentenceProcessChain
+  .reverse() // As said before bottom up request line
+  .forEach(processor => {
     nextDelegate = createDelegate(processor, nextDelegate);
-});
-
+  });
 
 // Start the process chain
 let helloWorldSentence = nextDelegate("Oh");
 
 // Show your work!
 console.log(helloWorldSentence);
-  
 
-const appDiv: HTMLElement = document.getElementById('app');
+const appDiv: HTMLElement = document.getElementById("app");
 appDiv.innerHTML = `<h1>${helloWorldSentence}</h1>`;
